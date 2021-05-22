c736765637a637d609a7c4b39d6cbb114dfbdabf
Abdelrahman <me@abdosaed.ml>
2021-05-22 07:04:29 +0200

    fix issue when fields come as String

--- src/middlewares/is_intl.js
+++ src/middlewares/is_intl.js
@@ -3,7 +3,13 @@ const errorThrower = require("../utils/error_thrower");
 module.exports = (field) => {
   return async (req, res, next) => {
     try {
-      const input = JSON.parse(req.body[field]); //{ "en": "test", "ar": "تجربة" }
+      //in some time when i use form-data
+      //fields come as String for this i need to parse it
+      const input =
+        typeof req.body[field] === "object"
+          ? req.body[field]
+          : JSON.parse(req.body[field]); //{ "en": "test", "ar": "تجربة" }
+
       req.body[field] = input;
       for (let i = 0; i < CONSTANT.LANGUAGES.length; i++) {
         if (!input[CONSTANT.LANGUAGES[i]]) {
