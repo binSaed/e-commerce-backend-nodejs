f4c45d66c2d0a6dacf163dd2cf81b8a976500ad9
Abdelrahman <me@abdosaed.ml>
2021-05-23 09:28:51 +0200

    catch uncaughtException

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const app = require("./app");
+const fs = require("fs");
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("start listen on port:" + port));
+
+process.on("uncaughtException", (err) => {
+  console.error(err.stack); // either logs on console or send to other server via api call.
+  fs.appendFileSync(path.join(__dirname, "./../", "access.log"), err.stack);
+  // process.exit(1);
+});
